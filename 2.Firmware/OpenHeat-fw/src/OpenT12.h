ea2aaff26e1dc79231445092839a226b77d51fd2
starstory <czstara12@gmail.com>
2024-01-11 15:32:59 +0300

    修改屏驱 关闭导致错误的中断 清理个人配置文件和构建文件

--- 2.Firmware/OpenHeat-fw/src/OpenT12.h
+++ 2.Firmware/OpenHeat-fw/src/OpenT12.h
@@ -28,46 +28,46 @@
 #include "Bitmap.h"
 #include "max6675.h"
 
-#define setbit(x, y)       x|=(1<<y)
-#define clrbit(x, y)       x&=~(1<<y)
-#define reversebit(x, y)   x^=(1<<y)
-#define getbit(x, y)       ((x) >> (y)&1)
+#define setbit(x, y) x |= (1 << y)
+#define clrbit(x, y) x &= ~(1 << y)
+#define reversebit(x, y) x ^= (1 << y)
+#define getbit(x, y) ((x) >> (y)&1)
 
-//注意Pin36与Pin39连接了内部霍尔传感器,不建议复用高敏感用途
-//另外，建议给ADC输入串联一个0.1uF的电容，有利于减少噪声
-#define LED_Pin       2
-#define BEEP_PIN      25
+// 注意Pin36与Pin39连接了内部霍尔传感器,不建议复用高敏感用途
+// 另外，建议给ADC输入串联一个0.1uF的电容，有利于减少噪声
+#define LED_Pin 2
+#define BEEP_PIN 25
 
-#define ROTARY_PIN1     27
-#define ROTARY_PIN2     14
-#define BUTTON_PIN      33
+#define ROTARY_PIN1 27
+#define ROTARY_PIN2 14
+#define BUTTON_PIN 33
 
-#define TIP_ADC_PIN   -1
-#define NTC_ADC       -1
-#define POWER_ADC_PIN  4   //GPIO4    40号脚
-#define CUR_ADC       -1
+#define TIP_ADC_PIN -1
+#define NTC_ADC -1
+#define POWER_ADC_PIN 4 // GPIO4    40号脚
+#define CUR_ADC -1
 
-#define PWM1_PIN      26
-#define PWM2_PIN      -1
-#define SW_PIN        -1
+#define PWM1_PIN 26
+#define PWM2_PIN -1
+#define SW_PIN -1
 
-//计算主电源电压
+// 计算主电源电压
 #define POWER_ADC_VCC_R1 10000
 #define POWER_ADC_R2_GND 1000
 
 #define CNSize 12
 #define SCREEN_COLUMN 128
-#define SCREEN_ROW    64
+#define SCREEN_ROW 64
 #define SCREEN_PAGE_NUM 8
 #define SCREEN_FONT_ROW 4
 
-//温度限制
+// 温度限制
 #define TipMaxTemp 300
 #define TipMinTemp 0
 
-//烙铁安装移除事件计时器冷却时间：防止事件临界值抖动
+// 烙铁安装移除事件计时器冷却时间：防止事件临界值抖动
 #define TipEvent_CoolTime 1500
-//到温提示声播放稳定期：防止温度没有稳定下来就播放到温音效
+// 到温提示声播放稳定期：防止温度没有稳定下来就播放到温音效
 #define TempToneStabilitytime 1000
 
 enum SYSLANG
@@ -98,10 +98,9 @@ enum MESSAGETYPE
     LOG_ERROR,
 };
 
-void Log(MESSAGETYPE type, char* s);
+void Log(MESSAGETYPE type, char *s);
 void SYS_Reboot(void);
 void About(void);
-
 
 extern uint64_t ChipMAC;
 extern char ChipMAC_S[19];
@@ -110,13 +109,13 @@ extern char CompileTime[20];
 extern MAX6675 thermocouple;
 extern BluetoothSerial SerialBT;
 extern OneButton RButton;
-extern U8G2_SSD1306_128X64_NONAME_F_HW_I2C Disp;
+extern U8G2_SH1106_128X64_NONAME_F_HW_I2C Disp;
 extern PID MyPID;
 extern KFP KFP_Temp;
 
 extern uint8_t DEBUG_MODE;
 
-extern char* TipName;
+extern char *TipName;
 
 extern float BootTemp;
 extern float SleepTemp;
@@ -141,7 +140,6 @@ extern bool BoostEvent;
 extern bool UnderVoltageEvent;
 
 extern bool PWM_WORKY;
-
 
 extern uint8_t PIDMode;
 extern uint8_t Use_KFP;
@@ -178,7 +176,6 @@ enum TEMP_CTRL_STATUS_CODE
     TEMP_STATUS_HOLD,
 };
 extern uint8_t TempCTRL_Status;
-extern char* TempCTRL_Status_Mes[];
-
+extern char *TempCTRL_Status_Mes[];
 
 #endif
